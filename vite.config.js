9a7dd0aa2d07fd76eea0276a9c390c0b53f13f85
NICKTO44 <kamito1809@gmail.com>
2025-11-24 11:13:57 -0500

    Fix audio paths and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-      base: '/RR/',
-// Cambia 'refer' por el nombre de tu repositorio
-
+  base: '/RR/',
 })
